3a00d0ab6468fa860f60992c8d694e293a6dafd7
ShaikMurad <shaikmurad786@gmail.com>
2023-01-19 08:57:14 +0530

    Testing netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import "./App.css";
-
 export default function App() {
+
+const names = ["Jaayu", "Daahil", "Paahil"]
+const users =
+[{name:"Jaayu",
+pic:"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg"
+},
+{name:"Daahil",
+pic:"https://i.pinimg.com/236x/f3/af/1c/f3af1ca137939bad4e7f8d95d96d96b6.jpg"
+},
+{name:"Paahil",
+pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMF7rNYRqdBhKmsTiW0pes2TrBJnzv7zqbjMp9W9J4cX4XK8jSeUmHBgHrgIt9AmANjxk&usqp=CAU",
+}]
+
   return (
     <div className="App">
-      <Msg name="Jaayu" />
-      <Msg name="Dahil" />
-      <Msg name="Pahil" />
+       {users.map((user)=>(
+        <Msg name= {user.name} pic={user.pic} />
+
+        ))}
+      <Msg 
+      name= "Jaayu" 
+      pic="https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg" />
+      <Msg 
+      name= "Daahil"
+      pic="https://i.pinimg.com/236x/f3/af/1c/f3af1ca137939bad4e7f8d95d96d96b6.jpg"/>
+      <Msg 
+      name= "Paahil"
+      pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMF7rNYRqdBhKmsTiW0pes2TrBJnzv7zqbjMp9W9J4cX4XK8jSeUmHBgHrgIt9AmANjxk&usqp=CAU"
+      />
+      
     </div>
   );
 }
-function Msg(props) {
-  const name = props.name;
-  return (
+function Msg({pic, name}){
+  return(
     <div>
-      <h1>
-        Hello, <span className="userColorchange">{name}</span>🤩😎💐
-      </h1>
-      <p>this is super Cool</p>
+      <img className="user-pic" src={pic} alt="{name}" />
+      <h1>Hello, <span className="user-name">{name}</span>🙌😎🤩💐</h1>
     </div>
+    
+
   );
 }
---
+++ src/notes.md
@@ -0,0 +1,15 @@
+const users =
+[
+{
+name:"Jaayu",
+pic :"https://wallpapers.com/images/featured/4co57dtwk64fb7lv.jpg",
+},
+{
+name:"Daahil",
+pic:"https://i.pinimg.com/236x/f3/af/1c/f3af1ca137939bad4e7f8d95d96d96b6.jpg",
+},
+{
+name:"Paahil",
+pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMF7rNYRqdBhKmsTiW0pes2TrBJnzv7zqbjMp9W9J4cX4XK8jSeUmHBgHrgIt9AmANjxk&usqp=CAU",
+},
+]
